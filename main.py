f45ba754067f82912807f99206a7cb3e0c1cfe32
RyderDe <felixaburaka@gmail.com>
2022-09-06 15:15:15 +0300

    added some functions

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-import pypdf, requests,html5
+import requests, re
 from bs4 import  BeautifulSoup as BS
 
+url = "https://www.tutorialspoint.com/assembly_programming/index.htm"
+headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0'
+}
+
+req= requests.get(url,headers=headers)
+data= req.content
+data= BS(data,"html.parser")
+
+#print (data.find_all("li").get("href"), end= " ")
+lists= data.findAll("ul",attrs={"class":re.compile("^toc")})
+
+for link in lists:
+    if link.findAll("a",attrs={"href":re.compile("^/assembly")}):
---
+++ w.py
@@ -0,0 +1,6 @@
+import os
+import subprocess as s
+
+#os.system("am start -n com.whatsapp/.0")
+
+s.call("am")
